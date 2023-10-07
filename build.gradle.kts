3cde061e7733feac25d1938bf764125ba536b29e
Felix Hennerich <66116721+BlueNitrox@users.noreply.github.com>
2023-10-07 10:19:31 +0200

    try again

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     //trick: for the same plugin versions in all sub-modules
     id("com.android.library").version("8.1.0").apply(false)
     kotlin("multiplatform").version("1.8.21").apply(false)
+    id("maven-publish")
 }
 
 tasks.register("clean", Delete::class) {
